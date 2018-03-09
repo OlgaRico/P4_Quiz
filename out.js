b81e821cc941fc1425ca0f478e292ee892c833e0
OlgaRico <36777108+OlgaRico@users.noreply.github.com>
2018-03-09 23:32:08 +0100

    Update out.js

--- out.js
+++ out.js
@@ -1,56 +1,50 @@
-const figlet = require('figlet');
 const chalk = require('chalk');
-
+const figlet = require('figlet');
 
 
 /**
  * Dar color a un string.
- * 
- * @param msg Es string al que hay que dar color.
- * @param color El color con el que pintar msg.
- * @retu {string} Devuelve el string msg con el color indicado.
+ *
+ * @param msg String al que hay que darle color.
+ * @param color Color con el que hay que pintar.
+ * @return {string} Devuelve el string con el color que se ha indicado.
  */
-const colorize = (msg, color) => {
-   
-   if (typeof color !== 'undefined'){
-     msg = chalk[color].bold(msg);
-   }
-   return msg;
- };
- 
-/**
- *  Escribe un mensaje de log.
- * 
- * @param msg El String a escribir.
- * @param color Color del texto.
- */
-const log = (msg, color) => {
-  console.log(colorize(msg, color));
+const colorize = (msg,color) => {
+    if (typeof color !== "undefined"){
+        msg=chalk[color].bold(msg);
+    }
+    return msg;
 };
-
-
+/**
+ * Escribe un mensaje de log.
+ *
+ * @param msg El string a escribir
+ * @param color del texto
+ */
+const log = (msg,color) => {
+    console.log(colorize(msg,color));
+};
 /**
  * Escribe un mensaje de log grande.
- * 
+ *
  * @param msg Texto a escribir.
  * @param color Color del texto.
  */
-const biglog = (msg, color) => {
-  log(figlet.textSync(msg, { horizontalLayout: 'full' }), color);
+const biglog = (msg,color) => {
+    log (figlet.textSync(msg,{ horizontalLayout: 'full'}),color);
 };
-
 /**
- * Escribe el mensaje de error emsg.
- * 
- * @param emsg Texto del mensaje de error.
+ * Escribe el mensaje de error.
+ *
+ * @param emsg  Texto del mensaje de error.
  */
 const errorlog = (emsg) => {
-  console.log(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}`);
+    console.log(`${colorize("Error","red")}: ${colorize(colorize(emsg,"red"),"bgYellowBright")}`);
 };
 
 exports = module.exports = {
-  colorize,
-  log,
-  biglog,
-  errorlog
+    colorize,
+    log,
+    biglog,
+    errorlog
 };
